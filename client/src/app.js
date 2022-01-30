42d6dc3572da3c1c72491ec53d6a67628e36f2b7
Prashantt423 <84055497+Prashantt423@users.noreply.github.com>
2022-01-30 18:56:50 +0530

    register,login,added

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,12 @@
 import './app.scss';
-import { Home } from './home/Home';
+// import { Home } from './pages/home/Home';
+// import Watch from './pages/watch/Watch';
+import Register from './pages/register/Register';
+import Login from './pages/login/Login';
 export default function app() {
   return (
     <div>
-      <Home />
+      <Login />
     </div>
   );
 }
