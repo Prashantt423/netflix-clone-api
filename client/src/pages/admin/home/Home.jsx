c8c052a25e8a4b567df7f0f21a96dabd1ff68497
Prashantt423 <84055497+Prashantt423@users.noreply.github.com>
2022-02-07 13:37:47 +0530

    add widgets,userList page

--- client/src/pages/admin/home/Home.jsx
+++ client/src/pages/admin/home/Home.jsx
@@ -2,14 +2,19 @@ import Sidebar from '../../../components/dashboard/sidebar/Sidebar';
 import Topbar from '../../../components/dashboard/topbar/Topbar';
 import InnerHomePage from '../dashBoardHomePage/InnerHomePage';
 import './home.scss';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import UserList from '../userList/UserList';
 export default function Home() {
   return (
-    <div>
+    <Router>
       <Topbar />
       <div className='container'>
         <Sidebar />
-        <InnerHomePage />
+        <Routes>
+          <Route exact path='/' element={<InnerHomePage />} />
+          <Route path='/users' element={<UserList />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
