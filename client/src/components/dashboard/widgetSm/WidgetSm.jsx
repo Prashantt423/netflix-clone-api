a7c20dffadad73907a412231303defaeef867758
Prashantt423 <84055497+Prashantt423@users.noreply.github.com>
2022-02-14 23:26:32 +0530

    api add

--- client/src/components/dashboard/widgetSm/WidgetSm.jsx
+++ client/src/components/dashboard/widgetSm/WidgetSm.jsx
@@ -1,6 +1,26 @@
 import './widgetSm.scss';
 import Visibility from '@mui/icons-material/Visibility';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 export default function WidgetSm() {
+  const [newUsers, setNewUsers] = useState([]);
+
+  useEffect(() => {
+    const getNewUsers = async () => {
+      try {
+        const res = await axios.get('/users?new=true', {
+          headers: {
+            token:
+              'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwZTZmYzQ2NDk0Mjc3MTYwNDg4MmMxNiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYyNTgzMjMxMSwiZXhwIjoxNjI2MjY0MzExfQ.ATXV-1TTWIGyVBttTQSf0erRWjsgZ8jHQv1ZsUixbng',
+          },
+        });
+        setNewUsers(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getNewUsers();
+  }, []);
   return (
     <div className='dashBWidgetSm'>
       <span className='dashBWidgetSmTitle'>New Join Members</span>
