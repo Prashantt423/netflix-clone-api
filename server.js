a630c54da8c01aa113a502eb9a14a5a9e737f092
Prashantt423 <84055497+Prashantt423@users.noreply.github.com>
2022-02-21 13:37:30 +0530

    update server.js

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ const MovieRoute = require('./routes/movies');
 const ListRoute = require('./routes/lists');
 dotenv.config();
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 // middleware
 app.use(express.json());
 app.use(helmet());
