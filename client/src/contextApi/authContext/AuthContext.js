fbbc36c07f2b260cf3ffc0135411ca5ded292ef0
Prashantt423 <84055497+Prashantt423@users.noreply.github.com>
2022-02-21 09:06:42 +0530

    final commit

--- client/src/contextApi/authContext/AuthContext.js
+++ client/src/contextApi/authContext/AuthContext.js
@@ -2,7 +2,10 @@ import AuthReducer from './AuthReducer';
 import { createContext, useEffect, useReducer } from 'react';
 
 const INITIAL_STATE = {
-  user: JSON.parse(localStorage.getItem('user')) || null,
+  user:
+    localStorage.getItem('user') !== 'undefined'
+      ? JSON.parse(localStorage.getItem('user'))
+      : null,
   isFetching: false,
   error: false,
 };
